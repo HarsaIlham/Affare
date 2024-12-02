29098eababf3aaadc6471c24d30d3ea01f54bfe7
WahyuJumahM <wahyujumahm@gmai.com>
2024-12-02 13:47:45 +0700

    testing git pertama

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,11 +1,20 @@
-<div class="bg-indigo-800 relative select-none lg:flex lg:items-stretch w-full">
-    <div class="container flex">
-        <div class="flex flex-no-shrink items-stretch h-12">
-            <p class="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">Ini navbar</p>
-        </div>
-        <div class="lg:flex lg:items-stretch lg:justify-end ml-auto">
-            <a href="{{ route('auth.login') }}" class="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">Login</a>
-            <a href="/" class="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">Home</a>
-        </div>
+<body class="bg-gray-50">
+  <!-- Navbar -->
+  <header class="bg-white shadow">
+    <div class="container mx-auto px-6 py-4 flex justify-between items-center">
+      <div class="flex items-center space-x-3">
+        <img src="/logo.png" alt="Logo" class="h-8">
+        <span class="text-blue-600 font-bold text-xl">Affare!</span>
+      </div>
+      <nav class="space-x-6 text-gray-600">
+        <a href="#" class="hover:text-blue-600">Eksplor</a>
+        <a href="#" class="hover:text-blue-600">Perusahaan</a>
+        <a href="#" class="hover:text-blue-600">Tersimpan</a>
+        <a href="#" class="hover:text-blue-600">Terdaftar</a>
+      </nav>
+      <div class="flex items-center space-x-4">
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-500 hover:text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h11M9 21V3m8 9l-3 3m0 0l3-3m-3 3V3" /></svg></a>
+        <img src="/profile.jpg" alt="Profile" class="h-8 w-8 rounded-full">
+      </div>
     </div>
-</div>
+  </header>
